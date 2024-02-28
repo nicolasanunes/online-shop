cbb4ac3956064509d8f049aa755937326e37e036
Nícolas Nunes <nicolasanunes@hotmail.com>
2024-02-28 11:03:29 -0300

    function findAllOrders created

---
+++ src/order/dto/list-order.dto.ts
@@ -0,0 +1,14 @@
+import { ListUserDto } from 'src/user/dto/list-user.dto';
+import { OrderEntity } from '../entities/order.entity';
+
+export class ListOrderDto {
+  id: number;
+  date: string;
+  user?: ListUserDto;
+
+  constructor(order: OrderEntity) {
+    this.id = order.id;
+    this.date = order.date.toString();
+    this.user = order.user ? new ListUserDto(order.user) : undefined;
+  }
+}
--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -10,11 +10,15 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderService } from './order.service';
 import { UserId } from '../decorator/user-id.decorator';
 import { OrderEntity } from './entities/order.entity';
+import { UserTypeEnum } from '../user/enum/user-type.enum';
+import { Roles } from '../decorator/role.decorator';
+import { ListOrderDto } from './dto/list-order.dto';
 
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
+  @Roles(UserTypeEnum.Admin, UserTypeEnum.User)
   @Post()
   @UsePipes(ValidationPipe)
   async createOrder(
@@ -24,8 +28,17 @@ export class OrderController {
     return this.orderService.createOrder(createOrderDto, userId);
   }
 
+  @Roles(UserTypeEnum.Admin, UserTypeEnum.User)
   @Get()
   async findOrdersByUserId(@UserId() userId: number) {
     return this.orderService.findOrdersByUserId(userId);
+  }
+
+  @Roles(UserTypeEnum.Admin)
+  @Get('/all')
+  async findAllOrders() {
+    return (await this.orderService.findAllOrders()).map(
+      (order) => new ListOrderDto(order),
+    );
   }
 }
